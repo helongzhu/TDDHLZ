2d2c3fade6b363bbb7c7e4263651a47d30afd20e
zheng <zheng>
2022-04-26 14:30:23 +0800

    Stop at charpter five 27

--- lists/views.py
+++ lists/views.py
@@ -4,5 +4,8 @@ from django.shortcuts import render
 
 # Create your views here.
 def home_page(request):
-    return render(request,'home.html')
+   
+    return render(request,'home.html',{
+        'new_item_text': request.POST.get('item_text','')
+    })
     #return HttpResponse('<html><title>To-Do lists</title></html>')
