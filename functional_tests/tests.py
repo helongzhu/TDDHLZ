3d7d43afe73fcac9b265e20212684d984046a35e
zheng <zheng>
2022-06-08 20:55:14 +0800

    Use Bootstrap to improve layout

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,4 +1,4 @@
-from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
@@ -6,13 +6,14 @@ from selenium.common.exceptions import WebDriverException
 
 MAX_WAIT = 10
 
-class NewVisitorTest(LiveServerTestCase):
+class NewVisitorTest(StaticLiveServerTestCase):
 
     def setUp(self):
         self.path = 'E:\anaconda\geckodriver.exe' #import
         self.browser = webdriver.Firefox()
         
     def tearDown(self):
+        self.browser.refresh()
         self.browser.quit() #close the webpage after functional test
     
     '''def check_for_row_in_list_table(self, row_text):
@@ -94,6 +95,26 @@ class NewVisitorTest(LiveServerTestCase):
         self.assertIn('Buy milk', page_text)
 
         #Satisfied, they both go back to sleep
+
+
+    def test_layout_and_styling(self):
+        #Edith goes to the home page
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
+
+        #She notices the input bos is nicely centered
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        #She strats a new list and sees the input is nicely centered there too
+        inputbox.send_keys('testing')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: testing')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] /2,
+            512,
+            delta = 10
+        )
+
 
     
     
